36617b47e94e4510bf3ecc23f23a40af5c259a12
francisco villa salmeron <frankvi@yahoo.com>
2022-04-03 22:48:25 -0600

    Done whit this assigment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+ function distanceFromHqInBlocks(streetNum){
+ return Math.abs(42 - streetNum)
+ }
+
+ function distanceFromHqInFeet(streetNum) {
+    return distanceFromHqInBlocks(streetNum)*264
+}
+
+function distanceTravelledInFeet(dest, start) {
+    return Math.abs(dest - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet (start, destination)
+    if (distanceInFeet < 400) {
+        return 0
+    }   else if (distanceInFeet >= 400 && distanceInFeet <=2000) {
+        const totalDistanceInFeet2 = distanceInFeet - 400
+        return totalDistanceInFeet2 * .02
+    }   else if (distanceInFeet > 2000 && distanceInFeet <=2500) {
+        return 25
+    }   else {
+        return "cannot travel that far"
+    }
+}
